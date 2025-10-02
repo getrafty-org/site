56553de0c520465ecf5b79ed96dac05d60d26b3d
sidosera <sidoserah@gmail.com>
2025-10-02 22:31:48 +0100

    wip

--- src/_includes/layout.jsx
+++ src/_includes/layout.jsx
@@ -52,7 +52,6 @@ export default function Layout({ children }) {
             </nav>
         </header>
         <main>
-            <a href="/index">cd ~/</a>
             {children}
         </main>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"/>
